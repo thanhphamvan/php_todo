a948c5c26762a5ad58f27d2b6a674b724b943dae
Thanh Pham Van <thanhpv@sandinh.net>
2017-11-28 14:22:50 +0700

    first init

---
+++ dbconfig.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thanhpv
+ * Date: 11/28/17
+ * Time: 12:40
+ */
+    $host = "127.0.0.1";
+    $user = "root";
+    $password = "";
+    $database = "php_tut";
+    mysql_connect($host, $user, $password);
+    mysql_select_db($database);
+
